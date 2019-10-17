e1f8ef63ffef614cfa6b3a5d2c0494b286ef9541
Ashley <mcmillan.ashley@gmail.com>
2019-10-17 07:57:19 -0400

    Lesson prep

---
+++ 201910/W1D4/mmmdonuts.js
@@ -0,0 +1,25 @@
+// we have some donuts, and we're gonna do stuff to em
+
+const donuts = [
+    {
+        type: "jelly",
+        daysOld: 2,
+        stale: null
+    },
+    {
+        type: "plain",
+        daysOld: 0,
+        stale: null
+    },
+    {
+        type: "glazed",
+        daysOld: 23,
+        stale: null
+    },
+    {
+        type: "mystery",
+        daysOld: 3,
+        stale: null
+    }
+
+];
